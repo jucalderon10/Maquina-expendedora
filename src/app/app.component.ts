e17d837bdcd4a842b194dfcd2c72f6d6b26d592a
jucalderon10 <jucalderon10@poligran.edu.co>
2021-05-24 21:45:40 -0500

    Maquina completada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Maquina';
+  codigo!: number;
+  dinero!: number;
+  cambio!: number;
+  mensaje!: string;
+  inventario: Array<number>; 
+  ventasProductos: Array<number>; 
+  codigosProductos: Array<number>; 
+  precios: Array<number>; 
+  productos: Array<string>;
+  productosDisponibles: Array <number>;
+  
+
+  constructor(){
+    this.codigo = 10001;
+    this.cambio = 0;
+    this.inventario = new Array(30,40,30,40);
+    this.ventasProductos = new Array (0,0,0,0);
+    this.codigosProductos = new Array (10001,10002,10003,10004);
+    this.precios = new Array (1200,1600,3000,2000);
+    this.productos = new Array ('Chocolatina JET x 12gr', 'Coca Cola x 250ml', 'Maní moto natural x 150gr', 'Papas Margarita natural x 150gr');
+    this.productosDisponibles = new Array (30,40,30,40);
+  }
+
+  funcionamiento(){
+  for (let i = 0; i <= this.codigosProductos.length; i++) {
+    if(this.codigosProductos[i] == this.codigo && this.precios[i] <= this.dinero){
+      if(this.inventario[i]-this.ventasProductos[i] > 0){
+        this.mensaje = this.productos[i] + ", disponible";
+        this.ventasProductos[i]= this.ventasProductos[i]+1;
+        this.productosDisponibles[i] = this.productosDisponibles[i]-1;
+        console.log(this.ventasProductos);
+        console.log(this.inventario);
+        console.log(this.productosDisponibles);
+        this.cambio = this.dinero - this.precios[i];
+      }
+      else{
+        this.mensaje = this.productos[i] + ", no disponible";
+      }
+    }
+    
+  }
+  
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+import {InputTextModule} from 'primeng/inputtext';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
